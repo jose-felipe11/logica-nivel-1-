1a3e104dfc93352300ec776c6f6ad4ff2d548941
jose-felipe11 <felipeplinio835@gmail.com>
2025-03-18 21:30:49 -0300

    Create 06.py

---
+++ 06.py
@@ -0,0 +1,8 @@
+#6. Menu - Elabore um programa que mostre o seguinte menu na tela:
+print("Cadastro de Clientes")
+print("0-fim")
+print("1-inclui")
+print("2-altera")
+print("3-exclui")
+print("4-consulta")
+print("opção:")
