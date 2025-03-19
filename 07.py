783543620d779560fd83a39731e08f0d14ac23c7
jose-felipe11 <felipeplinio835@gmail.com>
2025-03-18 21:53:54 -0300

    Create 07.py

---
+++ 07.py
@@ -0,0 +1,14 @@
+#   7. Pinheiro  - Implemente um programa que desenhe um "pinheiro" na tela, similar ao abaixo.
+#Enriqueça o desenho com outros caracteres, simulando enfeites.
+print("     @")
+print("    &x&")
+print("   *xxx*")
+print("   &xxxxx*")
+print("  *xxxxxxx&")
+print("  &xxxxxxxxx*")
+print(" *xxxxxxxxxxx*")
+print(" &xxxxxxxxxxxxx&")
+print("*XXXXXXXXXXXXXXX*")
+print("    {{{xx}}}")
+print("    {{{xx}}}")
+print("=======xxxx======")
