ebe77882ebf26d7e8cc698dddfeff9aa8e444dba
jose-felipe11 <felipeplinio835@gmail.com>
2025-03-18 21:12:33 -0300

    Create 02.py

---
+++ 02.py
@@ -0,0 +1,5 @@
+#2. Etiqueta - Elabore um programa que imprima seu nome na primeira linha, o estado em que você mora na segunda, e seu município na terceira.
+
+print("jose felipe plinio")
+print("rio de janeiro")
+print("rio de janeiro")
