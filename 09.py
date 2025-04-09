5bdaf09cf30cc253e8727d522f465d360dac3f5e
jose-felipe11 <felipeplinio835@gmail.com>
2025-04-08 21:56:22 -0300

    Create 09.py

---
+++ 09.py
@@ -0,0 +1,6 @@
+#9 - escreva um software que some dois numeros.
+print('digite a primeiro numero ')
+numero1=int(input())
+print("digite o segundo numero")
+numero2=int(input())
+print(numero1 + numero2)
