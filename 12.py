2d1c771c35ac1d8bd381a66f228f15cff4db945a
jose-felipe11 <felipeplinio835@gmail.com>
2025-04-22 21:08:52 -0300

    Create 12.py

---
+++ 12.py
@@ -0,0 +1,6 @@
+#12 - Escreva um algoritmo para ler as dimensões de um retângulo (base e altura), calcular e escrever a área do retângulo.
+print("digite a base de um retangulo")
+base=float(input())
+print("digite a altura do seu retangulo")
+altura= float(input())
+print("a area do seu retangulo e " , altura *base)
