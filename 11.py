ef43a08f8f963cb6b3f0d31123cbf9df440202f6
jose-felipe11 <felipeplinio835@gmail.com>
2025-04-22 21:03:46 -0300

    Create 11.py

---
+++ 11.py
@@ -0,0 +1,4 @@
+#11 - Escreva um algoritmo para ler um valor numérico (do teclado) e escrever (na tela) o seu antecessor
+print("digite um numero")
+n1=int(input())
+print("o antessesor do seu numero e", n1-1)
