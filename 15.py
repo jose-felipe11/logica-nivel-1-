8ec6ca7141b81806d5f8998984a3fd736eef8bb2
jose-felipe11 <felipeplinio835@gmail.com>
2025-04-29 20:40:11 -0300

    Create 15.py

---
+++ 15.py
@@ -0,0 +1,9 @@
+#15) Ler um valor e escrever se é positivo ou negativo (considere o valor zero como positivo). 
+print ("digite um valor")
+numero=float(input())
+if numero<0:
+    print ("este numero e negativo")
+elif numero >0:
+    print("este numero e positivo ")
+elif numero==0:
+    print("este numero e positivo")
